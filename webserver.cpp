bd0787d36e765f7b0febf43a04320ba8ab42810f
ajuniper <ajuniper@local.local>
2023-10-14 00:19:02 +0100

    tidy page and fix issues

--- webserver.cpp
+++ webserver.cpp
@@ -7,9 +7,11 @@
 #include "heatchannel.h"
 #include <WiFi.h>
 #include "outputs.h"
+#include "tempsensors.h"
 
 extern bool o_pump_on;
 extern bool o_boiler_on;
+extern int target_temp;
 
 static const char statuspage[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
@@ -19,16 +21,6 @@ static const char statuspage[] PROGMEM = R"rawliteral(
 <body>
   <h2>Boiler Control</h2>
   %STATUS%
-  <A HREF="/heat?ch=0&q=3600">Hot Water 1 hour</A><BR/>
-  <A HREF="/heat?ch=0&q=-1">Hot Water On</A><BR/>
-  <A HREF="/heat?ch=0&q=0">Hot Water Off</A><P/>
-  <A HREF="/heat?ch=1&q=3600">Heating 1 hour</A><BR/>
-  <A HREF="/heat?ch=1&q=7200">Heating 2 hours</A><BR/>
-  <A HREF="/heat?ch=1&q=10800">Heating 3 hours</A><BR/>
-  <A HREF="/heat?ch=1&q=-1">Heating On</A><BR/>
-  <A HREF="/heat?ch=1&q=0">Heating Off</A><P/>
-  <hr/>
-  %STATUS2%
 </body>
 </html>
 )rawliteral";
@@ -36,92 +28,129 @@ static const char statuspage[] PROGMEM = R"rawliteral(
 // Replaces placeholder with button section in your web page
 static String statuspage_processor(const String& var){
   time_t now = time(NULL);
-  //Serial.println(var);
+  String s;
+  char ch;
   if(var == "STATUS"){
-    String s = "<p/>";
     int i;
+    unsigned long t;
+    s+="<p><table border=\"1\">";
+    s+="<tr><th align=\"left\">Name</th><th align=\"left\">Timer</th><th align=\"left\">Target<br/>Temp</th><th align=\"left\">Actions</th><th align=\"left\">State</th></tr>";
     for(i=0; i<num_heat_channels; ++i) {
         if (!channels[i].getEnabled()) { continue ; }
+        ch = '0'+i;
+        // name
+        s += "<tr><th align=\"left\">";
         s += channels[i].getName();
-        s += ": ";
-        unsigned long t = channels[i].getTimer();
-        switch (t) {
-            case 0:
-                s += "Off";
-                break;
-            case -1:
-                s += "On";
-                break;
-            default: {
-                unsigned long d = t-now;
-                if (d < 60) {
-                    s += String(d) + "s";
-                } else {
-                    if (d > 3600) {
-                        s += String(int(d/3600)) + "h ";
+        // timer
+        s += "</th><td>";
+        if (!channels[i].getActive()) {
+            s+="Inactive";
+            // empty cell for target temp
+            s+="</td><td>";
+        } else {
+            // timer
+            t = channels[i].getTimer();
+            switch (t) {
+                case 0:
+                    s += "Off";
+                    // cooldown
+                    t = channels[i].getCooldown();
+                    switch (t) {
+                        case 0:
+                            // cooldown off, say nothing
+                            break;
+                        case -1: // shouldn't happen
+                            s += " (cooling)";
+                            break;
+                        default:
+                            s += " (cooling ";
+                            s+= String((t-now)/60);
+                            s+=")";
                     }
-                    s += String(int((d%3600)/60)) + "m";
+                    break;
+                case -1:
+                    s += "On";
+                    break;
+                default: {
+                    unsigned long d = t-now;
+                    if (d < 60) {
+                        s += String(d) + "s";
+                    } else {
+                        if (d > 3600) {
+                            s += String(int(d/3600)) + "h ";
+                        }
+                        s += String(int((d%3600)/60)) + "m";
+                    }
                 }
             }
+            // target temperature
+            s+="</td><td>";
+            s+=String(channels[i].targetTemp());
+
+        } // end not inactive
+        // actions
+        s+="</td><td>";
+        if (!channels[i].getActive()) {
+            s+="<a href=\"/heat?ch=";
+            s+=ch;
+            s+="&a=1\">Activate</A>";
+            // empty cell for output states
+            s += "</td><td>";
+        } else {
+            s+="<a href=\"/heat?ch="; s+=ch ; s+="&q=3600\">+1h</a> ";
+            s+="<a href=\"/heat?ch="; s+=ch ; s+="&q=7200\">+2h</a> ";
+            s+="<a href=\"/heat?ch="; s+=ch ; s+="&q=10800\">+3h</a> ";
+            s+="<a href=\"/heat?ch="; s+=ch ; s+="&q=-1\">On</a> ";
+            s+="<a href=\"/heat?ch="; s+=ch ; s+="&q=0\">Off</a> ";
+            s+="<a href=\"/heat?ch="; s+=ch ; s+="&a=0\">Disable</a> ";
+
+            // output states
+            s += "</td><td>";
+            s += (channels[i].getOutput()) ? "on" : "off";
+            s += " ";
+            s += (channels[i].getInputReady().pinstate()) ? "calls " : "";
+            s += (channels[i].getInputSatisfied().pinstate()) ? "satisfied " : "";
+            s += (channels[i].getSatisfied()) ? "SATon" : "SAToff";
         }
-        t = channels[i].getCooldown();
-        switch (t) {
-            case 0:
-                // cooldown off, say nothing
-                break;
-            case -1: // shouldn't happen
-                s += "cooldown on";
-                break;
-            default:
-                s += "cooldown ";
-                s+= String((t-now)/60);
-        }
-        s+=" target temp ";
-        s+=String(channels[i].targetTemp());
-        s+="<br/>";
+        s+="</td></tr>";
     }
 
-    s+="<p/>";
-
-    for(i=0; i<num_heat_channels; ++i) {
-        if (!channels[i].getEnabled()) { continue ; }
-        s += channels[i].getName();
-        s += ": ";
-        s += (channels[i].getOutput()) ? "on" : "off";
-        s += " ";
-        s += (channels[i].getInputReady().pinstate()) ? "calls" : "";
-        s += (channels[i].getInputSatisfied().pinstate()) ? "satisfied" : "";
-        s += " ";
-        s += (channels[i].getSatisfied()) ? "SATon" : "SAToff";
-        s+="<br/>";
-    }
-    
-    s += "System: pump ";
+    s+="</table><p><table border=\"1\">";
+    s += "<tr><td>Boiler</td><td>";
     switch (o_boiler_state) {
         case OUTPUT_OFF: s+="off"; break;
         case OUTPUT_COOLING: s+="cycling"; break;
         case OUTPUT_HEATING: s+="heating"; break;
     }
-    s += " boiler ";
+    s += "</td></tr><tr><td>Pump</td><td>";
     switch (o_pump_state) {
         case OUTPUT_OFF: s+="off"; break;
-        case OUTPUT_COOLING: s+="cycling"; break;
-        case OUTPUT_HEATING: s+="heating"; break;
+        case OUTPUT_COOLING: s+="cooling"; break;
+        case OUTPUT_HEATING: s+="running"; break;
     }
-    return s;
-  } else if(var == "STATUS"){
-    String s = "<p/>";
-    s += "WiFi SNR: " + String(WiFi.RSSI()) + "<br/>";
-    s += "Date: ";
+    for (i=0; i<num_temps; ++i) {
+        s += "</td></tr><tr><td>";
+        s += temperatures[i].getName();
+        s += "</td><td>";
+        int t = temperatures[i].getTemp();
+        if (t != 999) {
+            s += String(t)+"C ";
+        } else {
+            s += "--- ";
+        }
+    }
+    s += "</td></tr><tr><td>Target Temp</td><td>" + String(target_temp);
+    s += "</td></tr><tr><td>WiFi SNR</td><td>" + String(WiFi.RSSI());
+    s += "</td></tr><tr><td>Date</td><td>";
     struct tm timeinfo;
     getLocalTime(&timeinfo);
 #define dt_len 21
     char dt[dt_len];
     ssize_t l = strftime(dt,dt_len,"%F %T",&timeinfo);
     s += dt;
-    return s;
+    s += "</td></tr></table>";
   }
-  return String();
+  return s;
 }
 
 static void redirect_to_home (AsyncWebServerRequest *request) {
@@ -133,13 +162,20 @@ static void redirect_to_home (AsyncWebServerRequest *request) {
 static void web_set_heat (AsyncWebServerRequest *request) {
     String x;
     // GET /heat?ch=N&q=X where X is seconds to run
-    if (request->hasParam("q") && request->hasParam("ch")) {
+    if (request->hasParam("ch")) {
         x = request->getParam("ch")->value();
         int ch = x.toInt();
         if ((ch >= 0) && (ch <= num_heat_channels)) {
-            x = request->getParam("q")->value();
-            int i = x.toInt();
-            channels[ch].adjustTimer(i);
+            if (request->hasParam("q")) {
+                x = request->getParam("q")->value();
+                int i = x.toInt();
+                channels[ch].adjustTimer(i);
+            }
+            if (request->hasParam("a")) {
+                x = request->getParam("a")->value();
+                int i = x.toInt();
+                channels[ch].setActive(i);
+            }
         }
     }
     redirect_to_home(request);
@@ -169,7 +205,7 @@ static void web_set_target (AsyncWebServerRequest *request) {
                 y = "Target temperature ";
                 y+=channels[ch].getName();
                 y+=" is ";
-                y+=x;
+                y+= String(channels[ch].targetTemp());
                 response = request->beginResponse(200, "text/plain", y);
             }
         } else {
@@ -188,5 +224,5 @@ void webserver_setup() {
         request->send_P(200, "text/html", statuspage, statuspage_processor);
     });
     server.on("/heat", HTTP_GET, web_set_heat);
-    server.on("/targettemp", HTTP_GET, web_set_heat);
+    server.on("/targettemp", HTTP_GET, web_set_target);
 }
