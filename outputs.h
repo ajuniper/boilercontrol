d1d88d4661c924cc85af4300e61a99688d168d73
ajuniper <ajuniper@local.local>
2024-10-17 23:26:42 +0100

    make requested output state available

--- outputs.h
+++ outputs.h
@@ -1,5 +1,11 @@
 // system output states
+#pragma once
+#include "scheduledoutput.h"
+
 typedef enum t_output_state { OUTPUT_OFF, OUTPUT_COOLING, OUTPUT_HEATING };
 
 extern t_output_state o_boiler_state;
 extern t_output_state o_pump_state;
+
+extern ScheduledOutput o_boiler_on;
+extern ScheduledOutput o_pump_on;
