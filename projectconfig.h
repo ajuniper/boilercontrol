4804d587ee10db93e485d1c090a9498472afdadd
ajuniper <ajuniper@local.local>
2024-11-04 23:04:39 +0000

    add more items and disable them

--- projectconfig.h
+++ projectconfig.h
@@ -1,2 +1,10 @@
 // define this to reduce all timeouts to facilitate testing
-#define SHORT_TIMES 1
+//#define SHORT_TIMES 1
+
+// for binary input testing, pull down to activate
+// note no pullup on selected pins
+//#define MAINSDETECT_TESTMODE
+
+// are the display connections on the left or right when looking at it?
+// leave unset for right, define for left
+//#define display_conns_on_left
